37b04e17356904343520ca5ffa57757485dafac4
Patrycja13 <patrycja.gawel13@onet.pl>
2017-10-03 10:34:15 +0200

    sumuj 2

--- cpp/sumuj.cpp
+++ cpp/sumuj.cpp
@@ -10,25 +10,19 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-
+    int i ;
    int suma=0 ;
-   int a,b,c,d,e,f,g,h,j,k;
+   int liczba = 0;
    
-    cout<<"Podaj a :";cin>>a;
-    cout<<"Podaj b :";cin>>b;
-    cout<<"Podaj c :";cin>>c;
-    cout<<"Podaj d :";cin>>d;
-    cout<<"Podaj e :";cin>>e;
-    cout<<"Podaj f :";cin>>f;
-    cout<<"Podaj g :";cin>>g;
-    cout<<"Podaj h :";cin>>h;
-    cout<<"Podaj j :";cin>>j;
-    cout<<"Podaj k :";cin>>k;
+   for ( i=0; i<11; i++)
+{
     
-for (int i=10; i<11; i++) suma = suma +1 ;
-
-cout << "Suma :"<< a+b+c+d+e+f+g+h+j+k ;
-
+    
+    cout << "Podaj liczbę:";
+     cin >> liczba ;
+     suma+= liczba ;
+ }
+    cout << "Suma liczb :" << suma<<endl;
 
     return 0;
 }
