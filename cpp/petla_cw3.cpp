723467663386823d7822515b792bc07a00f2bea3
Patrycja13 <patrycja.gawel13@onet.pl>
2017-10-10 10:34:49 +0200

    petle

--- cpp/petla_cw3.cpp
+++ cpp/petla_cw3.cpp
@@ -7,9 +7,8 @@
 using namespace std;
 int main(int argc, char **argv)
 {
-
-    int n =0 ;
-	cin >> n ;
+    int n = 0;
+	cin >> n;
     cout <<" Podaj liczbę "<<endl;
     for(int i=0;i<=n;i++)
 	cout <<i*i<<endl;
