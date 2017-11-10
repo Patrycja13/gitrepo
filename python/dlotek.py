589bb33e0240b8599f724bdf7237162d0d765ecf
Patrycja13 <patrycja.gawel13@onet.pl>
2017-11-10 08:46:11 +0100

    python

--- python/dlotek.py
+++ python/dlotek.py
@@ -3,7 +3,9 @@
 
 import random
 
+
 def losuj(ileliczb, maksliczb):
+
     liczby = []  # pusta lista
 
     ile = 0  # ilość unikalnych liczb
@@ -14,26 +16,44 @@ def losuj(ileliczb, maksliczb):
             liczby.append(liczba)
             ile += 1
 
-    print(liczby)
+    #  print(liczby)
     return liczby
 
-def main(args):
-    ileliczb = int(input("Ile liczb chcesz zgadywać ?"))
-    maksliczb = int(input("Maksymalna losowana liczba:"))
 
-    liczby = losuj(ileliczb, maksliczb)
+def pobierz_typy(ileliczb):
+
     typy = set()
     # for i in range(ileliczb):
     ile = 0
     while ile < ileliczb:
-        typ = input("Podaj typ: ")
+        typ = int(input("Podaj typ: "))
         if typ not in typy:
             typy.add(typ)
             ile += 1
 
-    print(typy)
-    
+    return typy
+
+
+def main(args):
+
+    ileliczb = int(input("Ile liczb chcesz zgadywać ?"))
+    maksliczb = int(input("Maksymalna losowana liczba:"))
+
+    while ileliczb > maksliczb or ileliczb < 1:
+        ileliczb = int(input(' Ile liczb chcesz zgadywać z %s liczb ?' %
+                             maksliczb))
+
+    liczby = losuj(ileliczb, maksliczb)
+    typy = pobierz_typy(ileliczb)
+
+    #  print(liczby)
+    #  print(typy)
+    trafione = set(liczby) & typy
+
+    print('Trafiłeś ', len(trafione), 'liczb.')
+
     return 0
+
 
 if __name__ == '__main__':
     import sys
