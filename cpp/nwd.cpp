8398974e87903e8855367d7aa25ff7ea859a2b58
Patrycja13 <Patrycja.gawel13@onet.pl>
2017-11-20 17:05:51 +0100

    nwd

---
+++ cpp/nwd.cpp
@@ -0,0 +1,30 @@
+/*
+ * nwd.cpp
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int nwd(int a,int b)
+{
+    //int a,b;
+    while (a!=b) 
+        if (a > b)
+            a = a - b;
+        else 
+            b = b - a;
+    return a;
+ }           
+int main(int argc, char **argv)
+{
+	int a,b;
+    cout << "Podaj liczbę" << endl;
+    cin >> a ;
+    cout << "Podaj liczbę" << endl;
+    cin >> b ;
+    cout <<"Największy wspólny dzielnik: "<< nwd(a,b) <<endl;
+	return 0;
+}
+
