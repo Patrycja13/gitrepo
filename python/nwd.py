7c18be6c8dc939f74d942bf603b68378795df9f6
Patrycja13 <patrycja.gawel13@onet.pl>
2017-11-20 12:35:36 +0100

    nwd

--- python/nwd.py
+++ python/nwd.py
@@ -2,24 +2,31 @@
 # -*- coding: utf-8 -*-
 #
 #  nwd.py
-#  znaleźć największy wspólny dzielnik dwóch liczb naturalnych całkowitych 
-#  
+#  znaleźć największy wspólny dzielnik dwóch liczb naturalnych całkowitych
 
-def nwd(a,b):
-    while a!=b:
-        if a>b:
-            a=a-b
+
+def nwd_v1(a, b):
+
+    while a != b:
+        if a > b:
+            a = a - b
         else:
-            b=b-a
+            b = b - a
     return a
-            
+
+
 def main(args):
-    
-    a=int(input("Podaj liczbę"))
-    b=int(input("Podaj liczbę"))
-    
-    print("Największy spólny dzielnik ", nwd(a,b))
+
+    a = int(input("Podaj liczbę"))
+    b = int(input("Podaj liczbę"))
+    assert nwd_v1(5, 10) == 5
+    assert nwd_v1(2, 5) == 1
+    assert nwd_v1(4, 10) == 2
+    print ("NWD({:d}, {:d}) = {:d}".format(a, b, nwd_v1(a, b)))
+    print("Największy spólny dzielnik ", nwd_v1(a, b))
+
     return 0
+
 
 if __name__ == '__main__':
     import sys
