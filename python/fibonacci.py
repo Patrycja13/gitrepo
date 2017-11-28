344da690645e8e2490fb5c321d53441921c6fdc3
Patrycja13 <patrycja.gawel13@onet.pl>
2017-11-28 10:36:30 +0100

    fibonacci

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -8,18 +8,31 @@ def fib_iter(n):
     f(1) =1
     f(n) = F(n-2)+F(n-1)
     """
-    a, b = (0, 1)
+    """a, b = (0, 1)
     for i in range(1, n - 1):
         a = b
         b = a + b
     if n < 2:
         return b
     else:
-        return a
+        return a"""
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    a, b = (0, 1)  # lista niemodyfikowalna- 'tupla' lub krotka
+    print(a)
+    for i in range(1, n - 1):  # dla n-1 uwzgledniamy 0 w ciągu
+        tmp = b   # a, b = a, a +b może też być tak
+        b = a + b
+        a = tmp
+        print(a, " ", b, b / a)
+    return b
 
 
 def main(args):
-    n = int(input('podaj wyraz ciągu: '))
+    """n = int(input('podaj wyraz ciągu: '))
     assert fib_iter(0) == 0
     assert fib_iter(1) == 1
     assert fib_iter(2) == 1
@@ -27,8 +40,8 @@ def main(args):
     assert fib_iter(4) == 3
     assert fib_iter(5) == 5
     assert fib_iter(6) == 8
-    assert fib_iter(10) == 55
-    print("wyraz {:d} = {:d}".format(n, fib_iter(n)))
+    assert fib_iter(10) == 55"""
+    print("wyraz {:d} = {:d}".format(10, fib_iter(10)))
     return 0
 
 if __name__ == '__main__':
