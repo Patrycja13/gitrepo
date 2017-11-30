c61086454ff9bb7eabdf975f48fb160cfd2e58ab
Patrycja13 <Patrycja.gawel13@onet.pl>
2017-11-30 21:22:04 +0100

    fibonacci.

--- cpp/fibonacci.cpp
+++ cpp/fibonacci.cpp
@@ -6,25 +6,34 @@
 
 
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+
 
 void fib_iter(int n)
 {
     int a=0;
     int b=1;
+    //int wynik;
     
     for(int i=0;i<n;i++ )
     {
-        a=b;
-        a=a+b;
-        }
-    
+        
+        cout<<b<<" ";
+        b += a;
+        a = b-a;
+            // wynik = a + b;
+            // a = b;
+            //b = wynik;
+    }
 }
 int main(int argc, char **argv)
 {
 	int n;
     cout<<"Podaj liczbę"<<endl;
     cin>>n;
+    
+    fib_iter(n);
 	return 0;
 }
 
