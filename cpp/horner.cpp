82364a706d40ba79e82a5c6b54a2a19428240749
Patrycja13 <patrycja.gawel13@onet.pl>
2017-12-01 08:46:37 +0100

    Horner

---
+++ cpp/horner.cpp
@@ -0,0 +1,46 @@
+/*
+ * horner.cpp
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+// W(x) = 2X^3 + 3X^2 + 5X + 4 
+// W(x) = x(2x^2 + 3x + 5) + 4
+// W(x) = x (x (2x + 3) +5) +4
+
+float horner_it(int k, float tbwsp[],float x)
+{
+    int i;
+    float wynik = tbwsp[0];//tbwsp[0]-   0 - pierwsza wartość w tabeli
+    for (i=1;i<k+1;i++){
+    //k+1 bo bo k to 3 i skończyłoby się na 3 
+        wynik = wynik*x + tbwsp[i];
+    }
+    return wynik;
+}
+int main(int argc, char **argv)
+{
+    int stopien = 3;
+    float x;
+    float tbwsp[4];
+    int i;
+    
+    cout<<"Podaj wartość x "<<endl;
+    cin>> x;
+    
+    for(i=0; i<4 ;i++)// 2 człon oznacza ile razy !!!
+     {
+        cout<<"Podaj współczynniki ";
+        cin>>tbwsp[4];
+}
+    
+    cout<<horner_it(stopien,tabwsp,x)<<endl;
+    
+    
+	
+	return 0;
+}
+
