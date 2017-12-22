b9bb5960f6544aaf324ba2e7fd7822d01e20e763
Patrycja13 <patrycja.gawel13@onet.pl>
2017-12-22 08:44:52 +0100

    sortowanie

--- python/bubble.py
+++ python/bubble.py
@@ -6,35 +6,32 @@
 #  
 from random import randint
 
+
 def wypelnij(lista, ile, maks):
-    
-
     for i in range(ile):
-        lista.append(randint(0,maks))
-    return lista
-
-
-def sort_bubble(lista):
-    
-    
-	print(" ------------- Sortowanie bąbelkowe ---------------")
-	for i in range(len(lista)):
-		# k = i 
-		for j in range(i + 1, len(lista)):
-			if lista[j] > lista[j+1]:
-
-        lista[j], lista[j+1] = lista[j+1], lista[j]
+        lista.append(randint(0, maks))
+        ile += 1
         
     return lista
-    
-def main(args):
-    
-    lista = []
-    print(wypelnij(lista,10,20))
-    print(sort_bubble(lista))
-    return 0
 
+
+def sort_babel(lista):
+    print("----Sortowanie babel --------")
+    for i in range(len(lista)):
+        for j in range(len(lista) - 1):
+            if lista[j] > lista[j + 1]:
+                
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+
+    return lista
+    
+
+def main(args):
+    lista = []
+    print(wypelnij(lista, 10, 20))
+    print(sort_babel(lista))
+    return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
