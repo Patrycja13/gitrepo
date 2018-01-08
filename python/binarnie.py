4e0f30f65af4fe40100297202c38d4b5d6959d94
Patrycja13 <patrycja.gawel13@onet.pl>
2018-01-08 12:24:28 +0100

    binarne

--- python/binarnie.py
+++ python/binarnie.py
@@ -4,39 +4,52 @@
 from math import floor
 
 
-def wyszukiwanie_liniowe(l, e):
-
+def wyszuk_liniowe(l, e):
     for i in range(0, len(l)):
         if l[i] == e:
             return i
-    return -1  # 0 nie może być bo to poprawnym indeks tabeli,  musi być
-    # dowolna wartość niemożliwa dla indekssu czyli na -
-
-
-def wyszukiwanie_binarne(l, e):
-
-    lewy, prawy = 0, len(l - 1)  # 0 bo krańcowe po lewej stronie,
-    #  prawy - 1 bo indeksy startują od 0
-    while lewy < prawy:
-        srodek = floor(lewy + prawy) / 2
-        print(srodek)
-        if e <= 1[srodek]:
-            prawy = sredek
-        else:
-            lewy = srodek + 1
-
     return -1
 
 
-def main(args):
+def wyszuk_binarne(l, e):
+    lewy, prawy = 0, len(l) - 1
+    while lewy < prawy:
+        srodek = floor((lewy + prawy) / 2)
+        if e <= l[srodek]:
+            prawy = srodek
+        else:
+            lewy = srodek + 1
 
-    l = [4, 3, 7, 0, 2, 3, 1, 9, -4]
-    e = 1
-    print(wyszukiwanie_liniowe(l, e))
-    print(wyszukiwanie_binarne(l, e))
+    if l[lewy] == 0:
+        return lewy  # element znaleziono
+
+    return -1
+
+def wyszuk_bin_rek(lewy, prawy, lista, el):
+
+    if lewy < prawy:
+        return -1  # elementu nie znaleziono
+
+    srodek = floor((lewy + prawy) / 2)
+    if el == lista[srodek]:
+        return srodek  # element znaleziony
+
+    if el < lista[srodek]:
+        return wyszuk_bin_rek(lewy, srodek - 1, lista, el)
+    else:
+        return wyszuk_bin_rek(srodek + 1, prawy, lista, el)
+
+def main(args):
+    lista = [4, 3, 7, 0, 2, 3, 1, 9, -4]
+    e = 9
+    print(wyszuk_liniowe(lista, e))
+    lista.sort()
+    print(lista)
+    print(wyszuk_binarne(lista, e))
+    print(wyszuk_bin_rek(0, len(lista) - 1, lista, el))
     return 0
 
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
