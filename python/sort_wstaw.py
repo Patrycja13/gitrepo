25707c2de072b37e7525016664ccb5a9915376be
Patrycja13 <patrycja.gawel13@onet.pl>
2018-01-09 10:35:51 +0100

    sortowanie

--- python/sort_wstaw.py
+++ python/sort_wstaw.py
@@ -3,42 +3,49 @@
 #
 #  sort_wstaw.py
 
+from math import floor
+from random import random
 
-from random import randint
 
-
-def sort_wstaw(lista):
-    """wersja liniowa"""
-
+def sort_wst(lista):
     for i in range(1, len(lista)):
         el = lista[i]
-        k = i - 1  # k ma wskazać indeks na który należy wstawić pobrana z nieposortowanej strony(prawej) liczbę
-            while k >= 0 and lista[k] > el:  # < sortowanie malejące lub > rosnące
-            lista[k + 1] = lista[k]  # przesuwanie elementu
-            k = k - 1
-            lista[k + 1] = el
-
+        k = i - 1
+        while k >= 0 and lista[k] > el:
+            lista[k + 1] = lista[k]
+            k -= 1
+        lista[k + 1] = el
     return lista
 
-def sort_wstaw_bin(lista):
 
+def szukaj_bin(lewy, prawy, lista, el):
+    """Wersja liniowa wyszukiwania binarnego,
+    wyszukujemy indeks do wstawienia elementu """
+    while lewy < prawy:
+        srodek = floor((lewy + prawy) / 2)
+        if el <= lista[srodek]:
+            prawy = srodek
+        else:
+            lewy = srodek + 1
+    return lewy
+
+
+def sort_wst_bin(lista):
     for i in range(1, len(lista)):
         el = lista[i]
-        k = szukaj_bin(0, i, lista, el )  # wyszukiwanie binarne miejsca do wstawielnia el
-        pass
+        # wyszuk. bin. indeksu do wstawiania el
+        k = szukaj_bin(0, i, lista, el)
+        lista = lista[:k] + [el] + lista[k:i] + lista[i + 1:]
+        print (lista)
+    return lista
 
 def main(args):
-    lista = [4,3,7,0,2,3,1,9]
-        #[3,4,7,0,2,3,1,9]
-        #[3,4,7,0,2,3,1,9]
-        #[0,3,4,7,2,3,1,9]
-        #[0,2,3,4,7,3,1,9]
-        #[0,2,3,3,4,7,1,9]
-        #[0,1,2,3,3,4,7,9]
+    lista = [4, 3, 7, 0, 2, 3, 1, 9]
     print(lista)
-    print (sort_wstaw(lista))
+    print(sort_wst_bin(lista))
     return 0
+
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
