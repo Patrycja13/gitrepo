cba1df5d2b97cad05652e72410af6317fb333d54
Patrycja13 <patrycja.gawel13@onet.pl>
2018-01-16 10:36:15 +0100

    algorytm

---
+++ cpp/algorytm.cpp
@@ -0,0 +1,21 @@
+/*
+ * algorytm.cpp
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+    int a;
+	cout<<"Podaj liczbę"<<endl;
+    cin>>a;
+    
+    if (0 < a && a < 100)
+    for (int i = a>0 && int i = a<100, )    
+
+	return 0;
+}
+
