20b0b4ef7777c2886ca2dcccca9942ed743ece0d
Patrycja13 <patrycja.gawel13@onet.pl>
2018-01-22 12:32:38 +0100

    algorytmy

---
+++ cpp/Gawel_alg1.cpp
@@ -0,0 +1,33 @@
+/*
+ * algorytm.cpp
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+    int a;
+	cout<<"Podaj liczbę"<<endl;
+    cin>>a;
+    while (a>0 && a<100);
+        if (a>0 && a<100) 
+            break;
+        else 
+            
+    
+    for(int i=2; i = a ;i+=2) 
+        {
+         cout<< i << ""<<endl;
+         
+         if ();
+            cout<<"Liczba jest parzysta"<<a<<endl;
+        else
+            cout<<"Liczba jest nieparzysta" <<a<<endl;
+        }
+        
+	return 0;
+}
+
