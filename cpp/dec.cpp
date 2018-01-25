0f24f88dc5b38bd216dba57c1590f822a28a7407
Patrycja13 <patrycja.gawel13@onet.pl>
2018-01-25 08:45:48 +0100

    dec

---
+++ cpp/dec.cpp
@@ -0,0 +1,21 @@
+/*
+ * dec.cpp
+ * 
+ * Copyright 2018  <>
+ *
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+	int liczba=120;
+    do{
+        while()
+        }
+	return 0;
+}
+
