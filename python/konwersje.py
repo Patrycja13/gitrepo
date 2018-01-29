ffe41ef5a73418b20d4956bc321b814e1f3b1d02
Patrycja13 <Patrycja.gawel13@onet.pl>
2018-01-29 21:44:58 +0100

    konwersja

--- python/konwersje.py
+++ python/konwersje.py
@@ -28,18 +28,32 @@ def dec2other():
     """ to pojawi sie po kolei w {} """
 
 def konwersja2(liczba, podstawa):
-    """funkvja nkonwertuje liczbe o dowolnej podstawie na system 10"""
+    """funkvja konwertuje liczbe o dowolnej podstawie na system 10"""
+    liczba = []  # lista reszt
+    while liczba != 0:
+        reszta = liczba % podstawa  # obliczanie reszty z dzielenia
+        if reszta > 9:
+            reszta = chr(reszta + 55)
+        liczba > append(str(reszta))
+        liczba = int(liczba / podstawa)
 
+    liczba.reverse()  # odwrócona kolejność
+    return "".join(liczba)
 def other2dec():
     """Pobiera podstawe i liczbe od użytkownika"""
-
+    liczba = int(input("Podaj licdzbę: "))
+    podstawa = int(input("Podaj podstawę: "))
+    while podstawa < 2 or podstawa > 16:
+        podstawa = int(input("Podaj podstawę: "))
+    print("Wynik konwersji: {}({}) = {}(10)".format(
+        liczba, konwersja1(liczba, podstawa), podstawa))
 
 def main(args):
     print("Zamiana liczby dziesiętnej na liczbe o danej podstawie"
           "<2;16> lub odwrotnie.")
     dec2other()
     other2dec()
-    """program porzekształca liczbe o dowolnej podstawie
+    """program porzekształca liczbe o dowolnej podstawie"""
     return 0
 
 
