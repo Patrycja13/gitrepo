22ba57032229f514b9878a98ab638e1d05b59497
Patrycja13 <Patrycja.gawel13@onet.pl>
2018-02-07 21:49:09 +0100

    szyfr

--- python/szyfr_cezara.py
+++ python/szyfr_cezara.py
@@ -7,18 +7,38 @@ def szyfruj_cezar(tekst, klucz):
     klucz = klucz % 26
     szyfrogram = ""
     for znak in tekst:
-        znak = znak.upper()  # lower zmniejsza liczbę
-        ascii = ord(znak) + klucz  # kod ascii litery zastępującej
+        #  znak = znak.upper()  # lower zmniejsza liczbę
+        if ord(znak) > 64 and ord(znak) < 91:
+            ascii = ord(znak) + klucz  # kod ascii litery zastępującej
             if ascii > 90:
                 ascii -= 26
+        if ord(znak) > 96 and ord(znak) < 123:
+            ascii = ord(znak) + klucz
+            if ascii > 122:
+                ascii -= 26
+        if ord(znak) == 32:
+            ascii = 32
         szyfrogram += chr(ascii)
     return szyfrogram
-
 
 def deszyfruj(szyfrogram, klucz):
 
     tekst = ""
-    pass
+    for znak in szyfrogram:
+        #  tekst = tekst.upper()
+        if ord(znak) > 64 and ord(znak) < 91:
+            ascii = ord(znak) - klucz 
+            if ascii > 90:
+                ascii -= 26
+        if ord(znak) > 96 and ord(znak) < 123:
+            ascii = ord(znak) - klucz 
+            if ascii >122:
+                ascii -= 26
+        if ord(znak) == 32:
+            if ascii == 32:
+                ascii = 32
+            tekst += chr(ascii)
+    
     return tekst # w pętli zastanów się co zwraca , z jakim kodem mamy do czynienia itp
   # obsłuż spacje oraz małe i duże litery
 
