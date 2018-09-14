0d5c05027ed3bdf44ea7bd2b187b300842f277dd
Patrycja13 <patrycja.gawel13@onet.pl>
2018-09-14 13:23:50 +0200

    funkcje

--- python/funkcjee.py
+++ python/funkcjee.py
@@ -12,12 +12,22 @@ def wypelnij(lista, ile, maks):
         lista.append(random.randint(0, maks))
     return lista
 
+def drukuj(lista):
+    licznik = 0
+    for liczba in lista:
+        if not liczba % 2:
+            licznik += 1
+    print("Liczb parzystych ", licznik)
+            
+            
 def main(args):
     lista = []
     ile = 75
     maks = 100
     wypelnij(lista, ile, maks)
     print(lista )
+    drukuj(lista)
+    
     
     return 0
 
