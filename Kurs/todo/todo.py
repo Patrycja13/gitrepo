3371f1507a30862547e7ad78c5cda4e27a185f62
Patrycja13 <patrycja.gawel13@onet.pl>
2018-11-27 15:37:03 +0100

    kurs

--- Kurs/todo/todo.py
+++ Kurs/todo/todo.py
@@ -14,7 +14,12 @@ class Zadania(QWidget, Ui_Widget):
     def __init__(self, parent=None):
         super(Zadania, self).__init__(parent)
         self.setupUi(self)
-
+        
+        # self.logujBtn.clicked.connect(self.loguj)
+        self.koniecBtn.clicked.connect(self.koniec)
+    
+    def koniec(self):
+        self.close()
 
 if __name__ == '__main__':
     import sys
